c763a56ca98874ff8c94c8d460505bf99b7c9e69
ikemoto-s <ikemoto_s@mti.co.jp>
2022-09-25 17:48:15 +0900

    First commit

---
+++ sample/sample.js
@@ -0,0 +1,14 @@
+const { read, write } = require("./helper");
+
+console.log("start");
+
+const request = process.argv[2];
+if(request === "read") {
+ read();
+} else if(request === "write"){
+ write();
+}else {
+    console.error("エラー：readまたはwriteの引数を入れてください")
+}
+
+console.log("end");
